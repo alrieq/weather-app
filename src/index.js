980a4c4cdc17386a35cf8f745a5864ca23051cb4
alrieq <alrieq@live.com>
2023-05-18 00:07:07 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import Layouts from './components/Layouts'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
+export default function App(){
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Layouts/>}>
+      <Route index element={<Home/>} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='*' element={<NotFound />} />
+    </Route>
+  </Routes>
+  
+  </BrowserRouter>
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
